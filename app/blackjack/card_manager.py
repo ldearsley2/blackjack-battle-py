29c4a6c9c0c7d98a46a27ec82b14554959904f37
ldearsley2 <ldearsley2@gmail.com>
2025-03-02 15:16:56 +0000

    Card manager MVP

--- app/blackjack/card_manager.py
+++ app/blackjack/card_manager.py
@@ -1,11 +1,45 @@
+import random
 from app.blackjack.deck import DECK
 
 
 class CardManager:
     def __init__(self, decks: int):
+        """
+        Maintains deck of cards
+        :param decks:
+        """
         self.cards: list = []
+        self.played_cards: list = []
         self.decks: int = decks
+        self.populate_cards()
 
-    def populate_cards(self):
+    def populate_cards(self) -> None:
+        """
+        Populate CardManager's cards with 52 times decks cards
+        :return:
+        """
         for i in range(self.decks):
             self.cards.extend(DECK)
+
+    def play_card(self) -> str | None:
+        """
+        Select random card, add to played_cards and remove from cards. Returns the chosen card
+        :return:
+        """
+        try:
+            card = random.choice(self.cards)
+            self.cards.remove(card)
+            self.played_cards.append(card)
+            return card
+        except IndexError:
+            print("No cards in card list")
+            return None
+
+    def reset_cards(self):
+        """
+        Reset cards to initial state, remove all played cards
+        :return:
+        """
+        self.played_cards = []
+        self.cards = []
+        self.populate_cards()
--- tests/unit/test_card_manager.py
+++ tests/unit/test_card_manager.py
@@ -8,12 +8,18 @@ class TestCardManager(unittest.TestCase):
     def test_cm_init(self):
         card_manager = CardManager(decks=1)
 
+        expected = [
+            "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
+            "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",
+            "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
+            "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS"
+        ]
+
         self.assertEqual(1 ,card_manager.decks)
-        self.assertEqual([], card_manager.cards)
+        self.assertEqual(expected, card_manager.cards)
 
     def test_cm_populate_cards(self):
         card_manager = CardManager(decks=1)
-        card_manager.populate_cards()
 
         expected = [
             "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
@@ -26,7 +32,6 @@ class TestCardManager(unittest.TestCase):
 
     def test_populate_two_decks(self):
         card_manager = CardManager(decks=2)
-        card_manager.populate_cards()
 
         expected = [
             "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
@@ -40,3 +45,17 @@ class TestCardManager(unittest.TestCase):
         ]
 
         self.assertEqual(expected, card_manager.cards)
+
+    def test_play_card(self):
+        card_manager = CardManager(decks=1)
+        played = card_manager.play_card()
+
+        print(played)
+
+        self.assertEqual(51, len(card_manager.cards))
+        self.assertEqual(1, len(card_manager.played_cards))
+
+    def test_play_card_empty_list(self):
+        card_manager = CardManager(decks=0)
+
+        self.assertEqual(None, card_manager.play_card())
